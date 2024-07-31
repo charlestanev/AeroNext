e1ef40591d890d141c06b1becefbef4ca48f69a1
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-07-31 16:18:48 +0300

    chore: Add ProductCard component to Home page

---
+++ aero-next/app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductCard = () => {
+    return (
+        <div>
+            <button onClick={() => console.log('click')}></button>
+        </div>
+    )
+}
+
+export default ProductCard
