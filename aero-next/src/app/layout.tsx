96d8bcf4fe421787c2df7afb69ea340dd092a5d6
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-08-03 17:35:57 +0300

    chore: Update landing page metadata

--- aero-next/src/app/layout.tsx
+++ aero-next/src/app/layout.tsx
@@ -6,8 +6,8 @@ import clsx from "clsx";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "Aero Next Landing Page",
+  description: "Some Random Template",
 };
 
 export default function RootLayout({
