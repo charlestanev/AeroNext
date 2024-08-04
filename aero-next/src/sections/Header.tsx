55a4bce34431d69d8da1b5b07aa8a280a593e31d
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-08-04 10:26:25 +0300

    chore: Update header component with sticky positioning and menu icon

--- aero-next/src/sections/Header.tsx
+++ aero-next/src/sections/Header.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ArroRight from '@/assets/arrow-right.svg';
 import Logo from '@/assets/logosaas.png';
 import Image from "next/image"
-
+import MenuIcon from '@/assets/menu.svg';
 const Header = () => {
   return (
-    <header>
-      <div className='flex justify-center items-center py-3 bg-black text-white text-sm '>
-        <div className='flex justify-center items-center py-3 bg-black'>
+    <header className='sticky top-0'>
+      <div className='flex justify-center items-center py-3 bg-black text-white text-sm gap-3'>
+        <p className="text-white/60 hidden md:block">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        <div className='inline-flex gap-1 items-center'>
           <p>get started for free</p>
           <ArroRight className='h-4 w-4 inline-flex justify-center items-center' />
         </div>
@@ -15,8 +16,18 @@ const Header = () => {
 
       <div className='py-5'>
         <div className="container">
-          <Image src={Logo} alt='logo' height={40} width={40} />
-
+          <div className='flex items-center justify-between'>
+            <Image src={Logo} alt='logo' height={40} width={40} />
+            <MenuIcon className="h-5 w-5 md:hidden" />
+            <nav className='hidden md:flex gap-6 text-black items-center'>
+              <a href="#">About</a>
+              <a href="#">Features</a>
+              <a href="#">Customers</a>
+              <a href="#">Updates</a>
+              <a href="#">Help</a>
+              <button className='bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight'>get for free</button>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
