8761ec836d1fae8c6f2f73ea21c27e359884c39c
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-08-17 19:10:52 +0300

    chore: Update package.json and package-lock.json with clsx dependency

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,5 @@
+// import CheckIcon from '@assets/check.svg';
+
 const pricingTiers = [
   {
     title: "Free",
@@ -52,10 +54,38 @@ const pricingTiers = [
 
 export const Pricing = () => {
   return (
-    <section>
+    <section className="py-24">
       <div className="container">
         <h2 className="section-title">Pricing</h2>
-        <p className="section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ullam cum repudiandae modi fugiat maxime!</p>
+        <p className="section-description mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ullam cum repudiandae modi fugiat maxime!</p>
+        <div>
+          {pricingTiers.map(({
+            title,
+            monthlyPrice,
+            buttonText,
+            // popular,
+            // inverse,
+            features }) => (
+            <div>
+              <h3>{title}</h3>
+              <div>
+                <span>${monthlyPrice}</span>
+                <span>/month</span>
+              </div>
+              <button>{buttonText}</button>
+              <ul>
+                {features.map((feature) => (
+                  <li>
+                    {/* <CheckIcon /> */}
+                    <span>{feature}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+
+
+        </div>
       </div>
     </section>
   );
