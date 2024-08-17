924635482d627d652f858489a42eb873c90f69c3
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-08-17 14:28:16 +0300

    chore: Add ProductShowcase component to Home page

--- aero-next/src/sections/ProductShowcase.tsx
+++ aero-next/src/sections/ProductShowcase.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+import productImage from '@/assets/product-image.png';
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section>
+        <div className="container">
+          <div>Lorem, ipsum dolor.</div>
+          <h2>Lorem ipsum dolor sit amet.</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, facilis. Dolorum quo, mollitia necessitatibus rem ducimus libero? Ut, ipsa unde?</p>
+          <Image src={productImage} alt="Product Image" className=''/>
+        </div>
+    </section>
+  );
 };
