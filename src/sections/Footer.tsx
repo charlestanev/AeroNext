e7bc17684e37071170027cfcb3a244642548a908
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-08-18 12:09:23 +0300

    feat: Add Footer section to Home page

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,35 @@
+import Image from "next/image";
+import logo from "@/assets/logosaas.png";
+import SocialX from "@/assets/social-x.svg";
+import SocialInsta from "@/assets/social-insta.svg";
+import SocialLinkedIn from "@/assets/social-linkedin.svg";
+import SocialPin from "@/assets/social-pin.svg";
+import SocialYoutube from '@/assets/social-youtube.svg';
+
 export const Footer = () => {
-  return null;
+  return (
+    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+      <div className="container">
+        <div className="inline-flex relative before:content-[''] before:top-0 before:left-0 before:right-0 before:bottom-0 before:h-full before:w-full before:blur before:bg-gradient-to-r from-[#F87BFF] via-[#FB92CF] via-[#FFDD9B] via-[#C2F0B1] to-[#2FD8FE] before:absolute">
+          <Image src={logo} height={40} alt="SaaS logo" className="relative" />
+        </div>
+        <nav className="flex flex-col md:flex-row md:justify-center gap-6 mt-6">
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Careers</a>
+        </nav>
+        <div className="flex justify-center gap-6 mt-6">
+          <SocialX />
+          <SocialInsta />
+          <SocialLinkedIn />
+          <SocialPin />
+          <SocialYoutube />
+        </div>
+        <p className="mt-6">&copy; 2024 Your Company, Inc. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
