b4088db96bfdc2ed69ead488f8a16dcd4cf3c0df
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-08-18 17:38:01 +0300

    chore: Add lazy loading for images in Hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,13 +1,31 @@
+'use client';
+
 import React from 'react';
 import ArrowIcon from '@/assets/arrow-right.svg';
 import cogImage from '@/assets/cog.png';
 import cylinderImage from '@/assets/cylinder.png';
 import noodleImage from '@/assets/noodle.png';
 import Image from 'next/image';
+import { motion, useScroll, useTransform, useMotionValueEvent } from 'framer-motion';
+import { useRef } from 'react';
+import { log } from 'console';
 
 const Hero = () => {
+  const heroRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: heroRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
+  useMotionValueEvent(scrollYProgress, "change", (latestValue) =>
+    console.log(latestValue)
+  );
+
   return (
-    <section className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip'>
+    <section
+      ref={heroRef}
+      className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip'>
       <div className="container">
         <div className="md:flex items-center">
           <div className='md:w-[478px]'>
@@ -25,9 +43,41 @@ const Hero = () => {
             </div>
           </div>
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <Image src={cogImage} alt='Cog Image' className='md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0' />
-            <Image src={cylinderImage} alt='Cylinder Image' width={220} height={220} className='hidden md:block -top-8 -left-32 md:absolute' />
-            <Image src={noodleImage} alt='Noodle Image' width={220} className='hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]' />
+            <motion.img
+              src={cogImage.src}
+              alt='Cog Image'
+              className='md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0'
+              animate={{
+                translateY: [-30, 30],
+              }}
+              transition={{
+                repeat: Infinity,
+                repeatType: 'mirror',
+                duration: 4,
+                ease: 'easeInOut',
+              }}
+
+            />
+            <motion.img
+              src={cylinderImage.src}
+              alt='Cylinder Image'
+              width={220}
+              height={220}
+              className='hidden md:block -top-8 -left-32 md:absolute'
+              style={{
+                translateY: translateY,
+              }}
+            />
+            <motion.img
+              src={noodleImage.src}
+              alt='Noodle Image'
+              width={220}
+              className='hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]'
+              style={{
+                rotate: 30,
+                translateY: translateY,
+              }}
+            />
           </div>
         </div>
       </div>
