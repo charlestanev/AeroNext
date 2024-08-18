1f37f663f15f30605808e6de3febb423611e1387
ELMIR CHARLIE TANEV <51314768+charlestanev@users.noreply.github.com>
2024-08-18 18:13:11 +0300

    feat: Add scroll animation to ProductShowcase section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,11 +1,25 @@
+'use client';
 import Image from 'next/image';
 import productImage from '@/assets/product-image.png';
 import pyramidImage from '@/assets/pyramid.png';
 import tubeImage from '@/assets/tube.png';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 export const ProductShowcase = () => {
+    const sectionRef = useRef(null);
+    const {scrollYProgress} = useScroll({
+        target: sectionRef,
+        offset: ['start end', 'end start'],
+    });
+
+    const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
+
     return (
-        <section className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'>
+        <section
+            ref={sectionRef}
+            className='bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip'>
             <div className="container">
                 <div className='section-heading'>
                     <div className='flex justify-center'>
@@ -16,18 +30,22 @@ export const ProductShowcase = () => {
                 </div>
                 <div className='relative'>
                     <Image src={productImage} alt="Product Image" className='mt-10' />
-                    <Image
-                        src={pyramidImage}
+                    <motion.img
+                        src={pyramidImage.src}
                         alt="Pyramid Image"
                         height={262}
                         width={262}
-                        className='hidden md:blockabsolute -right-36 -top-32' />
-                    <Image
-                        src={tubeImage}
+                        className='hidden md:block absolute -right-36 -top-32' 
+                        style={{ translateY }}
+                        />
+                    <motion.img
+                        src={tubeImage.src}
                         alt="Pyramid Image"
                         height={248}
                         width={248}
-                        className='hidden md:blockabsolute bottom-24 -left-36' />
+                        className='hidden md:block absolute bottom-24 -left-36' 
+                        style={{ translateY }}
+                        />
 
                 </div>
             </div>
